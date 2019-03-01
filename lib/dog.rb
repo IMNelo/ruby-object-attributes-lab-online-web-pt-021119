a69b88753b26c8e3440304067426cc50c6040873
Agnelo Nelo Ribeiro <agnelo.ribeiro@gmail.com>
2019-03-01 05:46:52 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,18 @@
 class Dog
   def name=(name)
-    @name = name
+    @name = name    #sets dog's name
   end
   
-  def name
+  def name    #gets dog's name
     @name
+  end
+  
+  def breed(breed)
+    @breed = breed
+  end
+  
+  def breed
+    @breed
   end
 end
 
